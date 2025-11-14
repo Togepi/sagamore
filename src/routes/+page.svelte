5e58d22d180961e32d5181c2249ab211bb9c78af
Togepi <johannvdt@gmail.com>
2025-11-14 21:52:19 +0100

    first commit2

---
+++ src/routes/+page.svelte
@@ -0,0 +1,263 @@
+<main
+  class="max-w-5xl mx-auto pt-12 p-2"
+>
+  <article
+    class="md:grid md:grid-cols-2"
+  >
+    <img
+      src="/accueil.jpg"
+      alt="un aventurier"
+      class="order-2"
+    />
+    <section
+      class="p-5 flex flex-col justify-center"
+    >
+      <h1
+        class="text-4xl md:text-7xl font-bold tracking-tight mb-4"
+      >
+        SAGAMORE
+      </h1>
+      <p
+        class="text-lg font-semibold tracking-tight mb-8 font-med"
+      >
+        Le jeu de rôle grandeur nature
+      </p>
+      <h3 class="font-semibold">
+        Date :
+      </h3>
+      <p>14 au 16 août 2026</p>
+
+      <h3 class="mt-4 font-semibold">
+        Lieu:
+      </h3>
+      <p>
+        Au <a
+          href="https://maps.app.goo.gl/DBvwrqNsMvRu54288"
+          >Chalet de La Palestine</a
+        > dans la Vallée de joux
+      </p>
+      <p class="mt-4 text-sm">
+        Si vous êtes intéresséx à faire
+        PJ ou PNJ, vous pouvez déjà
+        envoyer un ptit mail :
+      </p>
+      <a
+        href="mailto:sagamoreinscription@gmail.com"
+        >sagamoreinscription@gmail.com</a
+      >
+    </section>
+  </article>
+  <img
+    src="/separator.jpg"
+    alt=""
+    class="mx-auto py-12"
+  />
+  <article class="p-5 text-center">
+    <h2>Synopsis</h2>
+    <p
+      class="text-lg mt-8 font-semibold tracking-tight max-w-3xl mx-auto"
+    >
+      Quatre Royaumes mais un seul
+      territoire, ce jeu est une lutte
+      eternelle. Incarnez l'esprit d'une
+      carte de jeu dans le royaume de
+      Sagamore. Dans cet univers, chaque
+      carte a une fonction bien précise.
+      Vous allez devoir gérer votre
+      royaume tout en luttant et
+      négociant avec les autres peuples.
+      Combat, diplomatie et tromperie
+      seront de mise.
+    </p>
+  </article>
+  <img
+    src="/separator.jpg"
+    alt=""
+    class="mx-auto py-12 md:pb-44"
+  />
+  <article
+    class="md:grid md:grid-cols-2"
+  >
+    <section>
+      <img
+        src="/coeur.jpg"
+        alt=""
+        class="max-h-96 mx-auto"
+      />
+    </section>
+    <section class="p-5">
+      <h2>Coeur</h2>
+      <p
+        class="text-lg font-med font-semibold tracking-tight max-w-xl"
+      >
+        Le pragmatisme de la réussite.
+      </p>
+      <br />
+      <p>
+        Je suis Cœur, pragmatique et
+        souveraine. La réussite est mon
+        étoile, et je trace ma route
+        sans détour.
+      </p>
+      <br />
+      <p>
+        Peu importent les moyens, je
+        touche toujours mon but.
+        Patiente comme le temps, habile
+        comme le vent, je tisse mes
+        plans dans le silence.
+      </p>
+      <br />
+      <p>
+        Mon royaume est le plus riche
+        des quatre, nourri par les
+        chiffres et les promesses, par
+        les dettes que je crée et les
+        coffres que je remplis. Car dans
+        mes mains, même l’abstrait
+        devient or.
+      </p>
+    </section>
+  </article>
+  <img
+    src="/separator.jpg"
+    alt=""
+    class="mx-auto py-12 md:py-44"
+  />
+  <article
+    class="md:grid md:grid-cols-2"
+  >
+    <section class="md:order-2">
+      <img
+        src="/losange.jpg"
+        alt=""
+        class="max-h-96 mx-auto"
+      />
+    </section>
+    <section class="p-5">
+      <h2>Losange</h2>
+      <p
+        class="text-lg font-med font-semibold tracking-tight max-w-xl"
+      >
+        Sorcellerie et tradition
+      </p>
+      <br />
+      <p>
+        Je suis Losange, tissée de
+        sorcellerie et de tradition. Je
+        vis là où les arbres murmurent
+        et les montagnes gardent leurs
+        secrets.
+      </p>
+      <br />
+      <p>
+        On me dit mystérieuse, on
+        m’accuse de maléfices, mais ce
+        ne sont que les ombres que
+        projette l’ignorance.
+      </p>
+      <br />
+      <p>
+        Mon territoire est rude, mais
+        mon cœur est ouvert. À quiconque
+        cherche refuge, je tends la
+        main, car l’accueil est ma magie
+        la plus ancienne.
+      </p>
+    </section>
+  </article>
+  <img
+    src="/separator.jpg"
+    alt=""
+    class="mx-auto py-12 md:py-44"
+  />
+  <article
+    class="md:grid md:grid-cols-2"
+  >
+    <section>
+      <img
+        src="/reine_pique.png"
+        alt=""
+        class="max-h-96 mx-auto"
+      />
+    </section>
+    <section class="p-5">
+      <h2>Pique</h2>
+      <p
+        class="text-lg font-med font-semibold tracking-tight max-w-xl"
+      >
+        Fierté et honneur
+      </p>
+
+      <p>
+        Je suis Pique, voix haute et
+        cœur ardent. Chez moi, chaque
+        mot claque comme une épée,
+        chaque regard peut devenir duel.
+      </p>
+      <br />
+      <p>
+        Je ne recule jamais devant le
+        combat, car l’honneur est mon
+        souffle. Mais pour celles et
+        ceux qui cherchent à grandir, je
+        suis patience, je suis
+        générosité.
+      </p>
+      <br />
+      <p>
+        Taquine et vive, j’aborde la vie
+        avec une légèreté tranchante et
+        un humour déconcertant, toujours
+        prêt à désarmer les cœurs.
+      </p>
+    </section>
+  </article>
+  <img
+    src="/separator.jpg"
+    alt=""
+    class="mx-auto py-12 md:py-44"
+  />
+  <article
+    class="md:grid md:grid-cols-2 pb-12"
+  >
+    <section class="md:order-2">
+      <img
+        src="/trefle.jpg"
+        alt=""
+        class="max-h-96 mx-auto"
+      />
+    </section>
+    <section class="p-5">
+      <h2>Trèfle</h2>
+      <p
+        class="text-lg font-med font-semibold tracking-tight max-w-xl"
+      >
+        Prosperité est richesse.
+      </p>
+      <br />
+      <p>
+        Je suis la voix des champs et
+        des greniers, gardienne des
+        moissons, fille des vents doux.
+      </p>
+      <br />
+      <p>
+        Je marche en paix, comme on
+        glisse entre les épis, mais
+        quand l’injustice me frappe, mon
+        peuple se lève en vague
+        furieuse, et la terre elle-même
+        tremble sous notre colère.
+      </p>
+      <br />
+      <p>
+        Pourtant, le reste du temps, je
+        chante avec les saisons, je
+        tisse la culture dans chaque
+        geste, et je transforme
+        l’ordinaire en fête.
+      </p>
+    </section>
+  </article>
+</main>
