c6c3343c3dd9d8cb6f9d596f60e436f7521a0ad6
Togepi <johannvdt@gmail.com>
2026-02-04 10:17:55 +0100

    lol2l

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,47 @@
 <main class="dark:bg-black">
-  <article>
-    <img src="/img/gn_sagamore.jpg" alt="carte de sagamore et logo" class="hidden md:block" />
-    <img src="/img/gn_sagamore_mobile.jpg" alt="carte de sagamore et logo" class=" md:hidden" />
-  </article>
-  <article class="bg-black p-12">
-    <section
-      class="max-w-7xl flex justify-center mx-auto"
+   <section
+      class="  rounded text-center p-1 font-bold text-sm bg-black text-white "
     >
       <p
-        class="text-white text-xl font-bold"
+        
       >
-        14-15 août à St Laurent France,
-        20 chf
-      </p>
+        14-15 août à St Laurent en France,
+        20 chf.
+       
+     Si vous voulez participez,
+        envoyez un message à Jojo</p>
     </section>
-    <section
-      class="max-w-7xl flex justify-center mx-auto"
-    >
-      <p
-        class="text-white text-xl font-bold"
-      >
-        Si vous voulez particicipez,
-        envoyez un message à Jojo
-      </p>
-    </section>
+  
+  <article class="text-center font-black pt-24 pb-12">
+   
+    <h1 class="font-aqui text-9xl">SAGAMORE</h1>
+    <h2 class="font-aqui text-6xl">Le jeu de role grandeur nature</h2>
+
   </article>
+
   <article
-    class="flex justify-center p-12 py-24"
+    class="flex justify-center p-12  "
   >
-    <iframe width="100%" class="aspect-video" src="https://www.youtube.com/embed/-lLUOTyDnqU?si=-Ar4utq-vIIXNgTv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="75%" class="aspect-video " src="https://www.youtube.com/embed/-lLUOTyDnqU?si=-Ar4utq-vIIXNgTv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   </article>
 </main>
+<article class="py-12 ">
+  <section class="text-3xl space-y-12  p-12 font-black font-aqui max-w-7xl mx-auto">
+                    <p> Au commencement, était l’Âge Sombre.</p>
+                    <p>Les ténèbres occupaient les terres, semant peur et chaos. Chaque peuple vivait dans la guerre et la misère.</p>
+                    <p>Mais la cheftaine Orbra, guerrière nomade, traversa les continents. Elle Alla rencontrer le sage dragon Provos. Elle fit un pacte, et acquit un pouvoir titanesque. Elle se transforma en un arbre gigantesque, imbu de magie. Tant que Orbra serait feuillu, le continent resterait en paix.</p>
+                    <p>Le Royaume Sagamore fût fondé, et l’Âge de Lumière commença. Le pays connut prospérité et sérénité pendant des milliers de saisons. </p>
+                    <p>Mais rien ne protège du temps, et aujourd’hui, L’Arbre Monde perdit sa dernière feuille. Les forces des ténèbres se rassemblent aux frontières et les armées des royaumes voisons se renforcent. Les rites magiques protecteurs ont été oubliés, c’est l’épée qui parlera.</p>
+                    <p>L’Âge de Lumière se termine, L’Âge de conquête commence</p>
+                        
+						</section>
+    </article>
 
 <div class=" w-full px-5 md:px-0">
 		<div class="prose mx-auto p-12">
-			<h1>Introduction</h1>
-			<h2>Pitch</h2>
-									<p class="font-aqui bg-black text-white p-12">Au commencement, était l’Âge Sombre. Les ténèbres occupaient les terres, semant peur et chaos. Chaque peuple vivait dans la guerre et la misère.  Mais la cheftaine Orbra, guerrière nomade, traversa les continents. Elle Alla rencontrer le sage dragon Provos. Elle fit un pacte, et acquit un pouvoir titanesque. Elle se transforma en un arbre gigantesque, imbu de magie. Tant que Orbra serait feuillu, le continent resterait en paix. Le Royaume Sagamore fût fondé, et l’Âge de Lumière commença. Le pays connut prospérité et sérénité pendant des milliers de saisons. Mais rien ne protège du temps, et aujourd’hui, L’Arbre Monde perdit sa dernière feuille. Les forces des ténèbres se rassemblent aux frontières et les armées des royaumes voisons se renforcent. Les rites magiques protecteurs ont été oubliés, c’est l’épée qui parlera. L’Âge de Lumière se termine, L’Âge de conquête commence
-						</p>
+	
+									
+      <h2>Note d'Intention</h2>
 			<h3>En Résumé</h3>
 			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita quasi neque fugit suscipit molestiae consequatur quod aliquam, recusandae facere laudantium delectus sit earum totam ab libero ipsa, sunt reiciendis tempora.</p>
 			<h3>lolas</h3>
